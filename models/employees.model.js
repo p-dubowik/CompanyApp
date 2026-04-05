ec969e51ea13789ce27daa9ea10f96bb1e7b1ed4
Paweł Dubowik <p.dubowik27@gmail.com>
2026-04-06 00:35:16 +0200

    Update employee model to validate department by ObjectId

--- models/employees.model.js
+++ models/employees.model.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const employeeSchema = mongoose.Schema({
     firstName: { type: String, required: true},
-    lastName: { type: String, required: true}
+    lastName: { type: String, required: true},
+    department: { type: ObjectId }
 })
 
 module.exports = mongoose.model('Employee', employeeSchema);
