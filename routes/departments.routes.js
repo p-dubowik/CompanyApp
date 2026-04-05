6e9add7ce7a671e2c30aa6d6bc2e019f77d32d2a
Paweł Dubowik <p.dubowik27@gmail.com>
2026-04-05 23:39:52 +0200

    Remake departments routes to make use of mongoose

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -1,78 +1,82 @@
 const express = require('express');
 const router = express.Router();
-const { ObjectId } = require('mongodb');
+const Department = require('../models/departments.model');
 
-router.get('/departments', (req, res) => {
-  req.db.collection('departments')
-    .find()
-    .toArray()
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => {
-      res.status(500).json({ message: err });
-    });
+router.get('/departments', async (req, res) => {
+  try {
+    res.json(await Department.find());
+  }
+  catch(err) {
+    res.status(500).json({ message: err });
+  }
 });
 
-router.get('/departments/random', (req, res) => {
-  req.db.collection('departmets')
-    .aggregate([{ $sample: { size: 1 } }])
-    .toArray()
-    .then((data) => {
-      res.json(data[0]);
-    })
-    .catch((err) => {
-      res.status(500).json({ message: err});
-    });
+router.get('/departments/random', async (req, res) => {
+  try {
+    const count = await Department.countDocuments();
+    const rand = Math.floor(Math.random() * count);
+    const dep = await Department.findOne().skip(rand);
+    if(!dep) res.status(404).json({ message: 'Not found'});
+    else res.json(dep);
+  }
+  catch(err) {
+    res.status(500).json({ message: err });
+  }
 });
 
-router.get('/departments/:id', (req, res) => {
-  req.db.collection('departments')
-    .findOne({ _id: ObjectId(req.params.id) })
-    .then((data) => {
-      if(!data) res.status(404).json({ message: 'Not found' });
-      else res.json(data);
-    })
-    .catch((err) => {
-      res.status(500).json({ message: err });
-    });
-});
-
-router.post('/departments', (req, res) => {
-  const { name } = req.body;
+router.get('/departments/:id', async (req, res) => {
   
-  req.db.collection('departments')
-    .insertOne({ name: name })
-    .then(() => {
-      res.json({ message: 'OK' });
-    })
-    .catch((err) => {
-      res.status(500).json({ message: err });
-    });
+  try {
+    const dep = await Department.findById(req.params.id);
+    if(!dep) res.status(404).json({ message: 'Not found' });
+    else res.json(dep);
+  }
+  catch(err) {
+    res.status(500).json({ message: err });
+  }
 });
 
-router.put('/departments/:id', (req, res) => {
+router.post('/departments', async (req, res) => {
+
+  try {
+    const { name } = req.body;
+    const newDepartment = new Department({ name: name });
+    await newDepartment.save();
+    res.json({ message: 'OK' });
+  }catch(err) {
+    res.status(500).json({ message: err.message });
+  };
+});
+
+router.put('/departments/:id', async (req, res) => {
   const { name } = req.body;
 
-  req.db.collection('departments')
-    .updateOne({ _id: ObjectId(req.params.id) }, { $set: { name: name }})
-    .then(() => {
+  try {
+    const dep = await Department.findById(req.params.id);
+    if(dep) {
+      await Department.updateOne({ _id: req.params.id }, { $set: {name: name }});
       res.json({ message: 'OK' });
-    })
-    .catch((err) => {
-      res.status(500).json({ message: err });
-    });
+    }
+    else res.status(404).json({ message: 'Not found... '});
+  }
+  catch(err) {
+    res.status(500).json({ message: err });
+  }
 });
 
-router.delete('/departments/:id', (req, res) => {
-  req.db.collection('departments')
-    .deleteOne({ _id: ObjectId(req.params.id) })
-    .then(() => {
-      res.json({ message: 'OK' })
-    })
-    .catch((err) => {
-      res.status(500).json({ message: err });
-    });
+router.delete('/departments/:id', async (req, res) => {
+  
+  try {
+    const dep = await Department.findById(req.params.id)
+    if(dep){
+      await Department.deleteOne({ _id: req.params.id });
+      res.json({ message: 'OK' });
+    }
+    else res.status(404).json({ message: 'Not found...' });
+  }
+  catch(err) {
+    res.status(500).json({ message: err });
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,45 +1,36 @@
 const express = require('express');
-const mongoClient = require('mongodb').MongoClient;
 const cors = require('cors');
-
+const mongoose = require('mongoose');
 
 const employeesRoutes = require('./routes/employees.routes');
 const departmentsRoutes = require('./routes/departments.routes');
 const productsRoutes = require('./routes/products.routes');
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api', departmentsRoutes);
+// app.use('/api', employeesRoutes);
+// app.use('/api', productsRoutes);
+
+app.use((req, res) => {
+  res.status(404).send({ message: 'Not found...' });
+})
 
 
-mongoClient.connect('mongodb://0.0.0.0:27017', { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
-  if (err){
-    console.log(err);
-  }
-  else {
-    console.log('Successfully connected to the database');
-    const db = client.db('companyDB');
+mongoose.connect('mongodb://0.0.0.0:27017/companyDB', { useNewUrlParser: true });
+const db = mongoose.connection;
 
-    const app = express();
+db.once('open', () => {
+  console.log('Connected to the database');
+});
+db.on('error', err => console.log('Error ' + err));
 
-    app.use(cors());
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));
 
-    //Establish db connection in all endpoints
-    app.use((req, res, next) => {
-      req.db = db;
-      next();
-    });
 
-    app.use('/api', employeesRoutes);
-    app.use('/api', departmentsRoutes);
-    app.use('/api', productsRoutes);
-
-    app.use((req, res) => {
-      res.status(404).send({ message: 'Not found...' });
-    })
-
-    app.listen('8000', () => {
-      console.log('Server is running on port: 8000');
-    });
-
-  }
+app.listen('8000', () => {
+  console.log('Server is running on port: 8000');
 });
